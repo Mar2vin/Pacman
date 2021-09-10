ec5366bee55358b3883e50a37026d425953345fd
Mar2vin <marvincherrier@gmail.com>
2021-09-10 09:32:50 +0200

    pacman avant rangement

--- js/pacman.js
+++ js/pacman.js
@@ -3,16 +3,29 @@ let pacman = {
     y : 2,
     direction : 0
 }
-let fantome = {
-    x : 5,
+let score = 0;
+
+let fantome = [
+   a = {
+    x : 10,
+    y : 12,
+    direction : 1
+},
+    b = {
+    x : 2,
     y : 3,
     direction : 0
-}
-let fantome1 = {
-    x : 5,
+},
+    c = {
+    x : 2,
     y : 4,
     direction : 0
-}
+},
+    d = {
+    x : 2,
+    y : 5,
+    direction : 0
+}];
 let grille = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 2, 2, 2, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2, 2, 0],
@@ -23,9 +36,9 @@ let grille = [
     [0, 2, 2, 2, 2, 0, 2, 2, 2, 0, 2, 2, 2, 0, 2, 2, 2, 2, 0],
     [0, 0, 0, 0, 2, 0, 0, 0, 2, 0, 2, 0, 0, 0, 2, 0, 0, 0, 0],
     [0, 1, 1, 0, 2, 0, 2, 2, 2, 2, 2, 2, 2, 0, 2, 0, 1, 1, 0],
-    [0, 0, 0, 0, 2, 0, 2, 0, 0, 2, 0, 0, 2, 0, 2, 0, 0, 0, 0],
-    [2, 2, 2, 2, 2, 2, 2, 0, 2, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2],  //MILIEU
-    [0, 0, 0, 0, 2, 0, 2, 0, 0, 2, 0, 0, 2, 0, 2, 0, 0, 0, 0],
+    [0, 0, 0, 0, 2, 0, 2, 0, 0, 1, 0, 0, 2, 0, 2, 0, 0, 0, 0],
+    [0, 2, 2, 2, 2, 2, 2, 0, 1, 1, 1, 0, 2, 2, 2, 2, 2, 2, 0],  //MILIEU
+    [0, 0, 0, 0, 2, 0, 2, 0, 0, 1, 0, 0, 2, 0, 2, 0, 0, 0, 0],
     [0, 1, 1, 0, 2, 0, 2, 2, 2, 2, 2, 2, 2, 0, 2, 0, 1, 1, 0],
     [0, 0, 0, 0, 2, 0, 2, 0, 0, 0, 0, 0, 2, 0, 2, 0, 0, 0, 0],
     [0, 2, 2, 2, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2, 2, 0],
@@ -34,13 +47,17 @@ let grille = [
     [0, 0, 2, 0, 2, 0, 2, 0, 0, 0, 0, 0, 2, 0, 2, 0, 2, 0, 0],
     [0, 2, 2, 2, 2, 0, 2, 2, 2, 0, 2, 2, 2, 0, 2, 2, 2, 2, 0],
     [0, 2, 0, 0, 0, 0, 0, 0, 2, 0, 2, 0, 0, 0, 0, 0, 0, 2, 0],
-    [0, 2, 2, 2, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2, 2, 0],
+    [0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 ];
 
 setInterval (tourdeJeu, 500);
 let plateau = document.querySelector('.plateau');
-for (let i in grille){
+document.body.addEventListener("keyup",directionPacman);
+
+function afficheGrille(){
+    plateau.innerHTML="";
+   for (let i in grille){
     for (let j in grille [i]){
         let monElement = document.createElement('div');
         console.log (grille[i][j]);
@@ -59,34 +76,144 @@ for (let i in grille){
     
     }
 }
+ 
+}
 
 function tourdeJeu(){
+    afficheGrille();
+    movePacman();
+    collisionPacman();
+    carnivorePacman();
     affichePacman();
     afficheFantome();
-    afficheGrille();
-
+    
+    afficheScore();
+    gagnant();
 }
 
 function affichePacman() {
     let monElement = document.createElement('div');
     monElement.className = "pacman";
     monElement.style.gridArea = pacman.y + "/" + pacman.x;
+    if (pacman.direction == 0){
+        monElement.style.transform = "rotate(0deg)";
+    }
+    if (pacman.direction == 1){
+        monElement.style.transform = "rotate(90deg)";
+    }
+    if (pacman.direction == 2){
+        monElement.style.transform = "rotate(180deg)";
+    }
+    if (pacman.direction == 3){
+        monElement.style.transform = "rotate(270deg)";
+    }
+    
     plateau.appendChild(monElement);
 }
-function afficheFantome() {
-    let monElement = document.createElement('div');
-    monElement.className = "fantome";
-    monElement.style.gridArea = fantome.y + "/" + fantome.x;
-    plateau.appendChild(monElement);
+function movePacman(){
+    if (pacman.direction == 0){
+        pacman.x++;
+    }
+    if (pacman.direction == 1){
+        pacman.y++;
+    }
+    if (pacman.direction == 2){
+        pacman.x--;
+    }
+    if (pacman.direction == 3){
+        pacman.y--;
+    }
 }
-function afficheFantome1() {
-    let monElement = document.createElement('div');
-    monElement.className = "fantome1";
-    monElement.style.gridArea = fantome1.y + "/" + fantome1.x;
-    plateau.appendChild(monElement);
+function collisionPacman(){
+    if (grille[pacman.y -1][pacman.x -1] == 0){
+        if (pacman.direction == 0){
+            pacman.x--;
+        }
+        if (pacman.direction == 1){
+            pacman.y--;
+        }
+        if (pacman.direction == 2){
+            pacman.x++;
+        }
+        if (pacman.direction == 3){
+            pacman.y++;
+        }
+    }
+}
+function directionPacman(event){
+    if (event.key == "ArrowUp"){
+        pacman.direction= 3;
+    
+    }
+    if (event.key == "ArrowDown"){
+        pacman.direction= 1;
+       
+    }
+    if (event.key == "ArrowRight"){
+        pacman.direction= 0;
+      
+    }
+    if (event.key == "ArrowLeft"){
+        pacman.direction= 2;
+     
+    }
+}
+function carnivorePacman(){
+    if (grille[pacman.y -1][pacman.x -1] == 2){
+        grille[pacman.y -1][pacman.x -1] = 1;
+        score++;
+    }
+    
+}
+function afficheScore(){
+    let divScore=document.querySelector(".score");
+    divScore.innerHTML="SCORE :"+score;
+}
+function gagnant (){
+    if (score == 192){
+        window.alert("Vous avez gagné")
+    }
 }
 
- /*setInterval(function() {
+function afficheFantome() {
+    for(let i in fantome ){
+let monElement = document.createElement('div');
+    if(i == 0 ){
+        monElement.className = "fantome";
+    }
+    if(i == 1 ){
+        monElement.className = "fantome1";
+    }
+    if(i == 2 ){
+        monElement.className = "fantome2";
+    }
+    if(i == 3 ){
+        monElement.className = "fantome3";
+    }
+    monElement.style.gridArea = fantome[i].y + "/" + fantome[i].x;
+    plateau.appendChild(monElement);
+    }
+}
+
+/*function collisionFantome(){
+    if (grille[fantome.y -1][fantome.x -1] == 0){
+        if (fantome.direction == 0){
+            fantome.x--;
+        }
+        if (fantome.direction == 1){
+            fantome.y--;
+        }
+        if (fantome.direction == 2){
+            fantome.x++;
+        }
+        if (fantome.direction == 3){
+            fantome.y++;
+        }
+    }
+}
+
+
+ setInterval(function() {
    var dt=new Date();
    var heure=dt.getHours()+":"+dt.getMinutes()+":"+dt.getSeconds();
    document.getElementById("monHorloge").innerHTML = heure
